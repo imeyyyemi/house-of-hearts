7f6e4fa3361171cdc8538ca38cca55a0a8b84bff
jisody2004 <jisody2004@gmail.com>
2025-10-23 17:17:08 +0800

    chore: commit and push

--- src/main/java/com/example/onlinequiz/OnlineQuizApplication.java
+++ src/main/java/com/example/onlinequiz/OnlineQuizApplication.java
@@ -3,4 +3,4 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineQuizApplication {public static void main(String[] args) {
-SpringApplication.run(OnlineQuizApplication.class, args); }}
+SpringApplication.run(OnlineQuizApplication.class, args); } }
