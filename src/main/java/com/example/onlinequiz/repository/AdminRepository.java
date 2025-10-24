1b30c36c6888924b7a8f7ec8a2ae084b439c5d4c
jeanjade16 <jeanjadejagonoy@gmail.com>
2025-10-24 17:28:00 +0800

    chore: commit and push

--- src/main/java/com/example/onlinequiz/repository/AdminRepository.java
+++ src/main/java/com/example/onlinequiz/repository/AdminRepository.java
@@ -1,4 +1,4 @@
 package com.example.onlinequiz.repository; import com.example.onlinequiz.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface AdminRepository extends JpaRepository<Admin, Long> { AdminRepository
+public interface AdminRepository extends JpaRepository<Admin, Long> { Admin
 findByusername(String username); }
