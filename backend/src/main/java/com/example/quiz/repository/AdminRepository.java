5d4816eccc41cee1eb7b83b8a00f9664e8ec6ec1
imeyyyemi <imeylonganilla@ph.ibm.com>
2025-11-03 04:25:18 +0800

    Update AdminRepository.java

--- backend/src/main/java/com/example/quiz/repository/AdminRepository.java
+++ backend/src/main/java/com/example/quiz/repository/AdminRepository.java
@@ -4,5 +4,5 @@ import com.example.quiz.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Admin findByUsername(String username);
+   
 }
